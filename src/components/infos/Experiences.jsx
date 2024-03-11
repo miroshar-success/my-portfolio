b19312870bcd54e63d4fbd1cc90b768a6992251b
Oskar Mast <oskarmast9436@gmail.com>
2024-03-11 14:19:25 +0100

    add-experiences-json

---
+++ src/components/infos/Experiences.jsx
@@ -0,0 +1,149 @@
+export const experiences = [
+  {
+    company: "Sapien from Napa California",
+    role: "Senior Frontend Developer",
+    location: "Remote",
+    duration: "4 years",
+    tech_stack: [
+      {
+        name: "React.js",
+        bg_color: "indigo",
+        text_color: "green",
+      },
+      {
+        name: "Next.js",
+        bg_color: "green",
+        text_color: "yellow",
+      },
+      {
+        name: "Node.js",
+        bg_color: "sky",
+        text_color: "gray",
+      },
+      {
+        name: "TailWindCSS",
+        bg_color: "violet",
+        text_color: "blue",
+      },
+      {
+        name: "MongoDB",
+        bg_color: "purple",
+        text_color: "indigo",
+      },
+      {
+        name: "RESTful API",
+        bg_color: "fuchsia",
+        text_color: "pink",
+      },
+      {
+        name: "GitHub",
+        bg_color: "pink",
+        text_color: "red",
+      },
+    ],
+    responsibilities: [
+      "Developed Online Education Platfom as a Senior Frontend developer. Key features were lecture capture, course enrollment, exam management, discussion forums, and video conferencing. Especially I implemented features screen sharing, live chat, and collaborative whiteboarding to enhance real-time engagement and virtual classroom experiences using Next.js(version 14). The main role was frontend developing as a frontend developer.",
+      "Led frontend development efforts for various projects, emphasizing React.js to deliver highly responsive and intuitive user interfaces.",
+      "Spearheaded the migration of legacy frontend code to React.js, resulting in improved performance and maintainability across the platform.",
+      "Collaborated with cross-functional teams to implement Git workflows, ensuring efficient code management and version control.",
+      "Implemented RESTful APIs(payment, auth) to enable seamless communication between frontend(React built) and backend systems.",
+      "Resolved technical challenges by coordinating closely with team members, such as debugging complex UI issues and optimizing application performance.",
+    ],
+  },
+  {
+    company: "InsureDAO from Singapore",
+    role: "Frontend Developer",
+    location: "Remote",
+    duration: "2 years 6 months",
+    tech_stack: [
+      {
+        name: "React.js",
+        bg_color: "red",
+        text_color: "green",
+      },
+      {
+        name: "Next.js",
+        bg_color: "green",
+        text_color: "yellow",
+      },
+      {
+        name: "Node.js",
+        bg_color: "blue",
+        text_color: "white",
+      },
+      {
+        name: "TailWindCSS",
+        bg_color: "yellow",
+        text_color: "blue",
+      },
+      {
+        name: "MongoDB",
+        bg_color: "red",
+        text_color: "gray",
+      },
+      {
+        name: "RESTful API",
+        bg_color: "red",
+        text_color: "green",
+      },
+      {
+        name: "GitHub",
+        bg_color: "gray",
+        text_color: "green",
+      },
+    ],
+    responsibilities: [
+      "As a frontend developer, I developed E-commerce Dashboard. Key features were order management, inventory tracking, customer analytics, sales reporting, user authentication and authorization.",
+      "Played a key role in frontend development using React.js, focusing on crafting engaging user experiences and interactive interfaces.",
+      "Collaborated with backend developers to integrate frontend components with PHP/Node.js backend services, ensuring seamless data flow and functionality.",
+      "Implemented Git branching strategies to enable efficient collaboration and code management within the development team.",
+      "Leveraged AWS services for hosting and managing web applications, ensuring scalability and high availability.",
+      "Developed and consumed RESTful APIs to enable data exchange between frontend and backend systems.",
+      "Successfully resolved technical challenges by working closely with team members, such as optimizing page load times(using React hook) and enhancing UI responsiveness.",
+    ],
+  },
+  {
+    company: "Angry Nerds from Wrocław Poland",
+    role: "Junior Web Developer",
+    location: "Remote",
+    duration: "9 months",
+    tech_stack: [
+      {
+        name: "HTML",
+        bg_color: "red",
+        text_color: "green",
+      },
+      {
+        name: "CSS",
+        bg_color: "green",
+        text_color: "yellow",
+      },
+      {
+        name: "JavaScript",
+        bg_color: "blue",
+        text_color: "white",
+      },
+      {
+        name: "Node.js",
+        bg_color: "yellow",
+        text_color: "blue",
+      },
+      {
+        name: "React.js",
+        bg_color: "red",
+        text_color: "red",
+      },
+      {
+        name: "RESTful API",
+        bg_color: "red",
+        text_color: "green",
+      },
+    ],
+    responsibilities: [
+      "Gained foundational experience in web development using PHP and Node.js technologies.",
+      "Contributed to frontend development efforts using HTML, CSS, and JavaScript, laying the groundwork for subsequent specialization in React.js.",
+      "Assisted in the deployment of web applications on AWS infrastructure, gaining exposure to cloud-based hosting environments.",
+      "Participated in the development of RESTful APIs to support frontend and backend integration, ensuring smooth data exchange and functionality.",
+    ],
+  },
+];
