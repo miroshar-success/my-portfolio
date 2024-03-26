f14b82f9bf00de7db41aec58b2099a37dfcb8818
mirror0000 <poxii2001@gmail.com>
2024-03-26 10:01:22 +0100

    fix: tailwind css configuration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'tailwindcss/tailwind.css'; // Assuming your setup supports importing CSS files in JavaScript
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
