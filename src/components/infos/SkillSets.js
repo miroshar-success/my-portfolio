e58ef4b5e09d9da13bab70a69398751e3efade06
mirror0000 <poxii2001@gmail.com>
2024-03-26 01:27:30 +0100

    add: skillsets json data

---
+++ src/components/infos/SkillSets.js
@@ -0,0 +1,16 @@
+export const frontendSkills = {
+    title: 'In Frotnend',
+    skills: ['React.js', 'Next.js', 'JavaScript', 'TypeScript', 'BootStrap', 'Tailwind CSS', 'MUI'],
+}
+export const backendSkills = {
+    title: 'In Backend',
+    skills: ['Node.js', 'Express.js', 'PHP', 'MySQL', 'MongoDB', 'MSSQL', 'PostgreSQL'],
+}
+export const apis = {
+    title: 'In API',
+    skills: ['Whatsapp API', 'PayPal API', 'Authorize.Net API', 'Stripe API', 'SendGrid API', 'RESTful API'],
+}
+export const someKnowledge = {
+    title: 'Some Knowledge',
+    skills: ['SEO', 'Git', 'Docker', 'CI/CD', 'XDdesign', 'Figma', 'Web Socket'],
+}
