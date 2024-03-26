63334279aaf8a188eb5e1bd342ad95c19eb35733
mirror0000 <poxii2001@gmail.com>
2024-03-26 01:27:04 +0100

    Add: marquessSkills component

---
+++ src/components/infos/MarqueeSkills.js
@@ -0,0 +1,55 @@
+import {
+    DiHtml5,
+    DiCss3,
+    DiJavascript,
+    DiReact,
+} from "react-icons/di";
+import {
+    SiNextdotjs,
+    SiNodedotjs,
+    SiCodeigniter
+} from "react-icons/si";
+import { FaGithub, FaLaravel, FaWordpress } from "react-icons/fa";
+
+export const skillsets = [
+    {
+        name: 'HTML',
+        tag: DiHtml5,
+    },
+    {
+        name: 'CSS',
+        tag: DiCss3,
+    },
+    {
+        name: 'JavaScript',
+        tag: DiJavascript,
+    },
+    {
+        name: 'React.js',
+        tag: DiReact,
+    },
+    {
+        name: 'Next.js',
+        tag: SiNextdotjs,
+    },
+    {
+        name: 'Node.js',
+        tag: SiNodedotjs,
+    },
+    {
+        name: 'Laravel',
+        tag: FaLaravel,
+    },
+    {
+        name: 'Codeigniter',
+        tag: SiCodeigniter,
+    },
+    {
+        name: 'WordPress',
+        tag: FaWordpress,
+    },
+    {
+        name: 'GitHub',
+        tag: FaGithub,
+    },
+];
