c88d6f91d493b232795668537c709f7cb4385ad1
oskarmast <oskarmast9436@gmail.com>
2024-04-26 18:07:40 +0200

    add:images

--- src/components/topSection.js
+++ src/components/topSection.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import avatar from "./images/talent.jpg";
 const Section = () => {
   return (
-    <div className="lg:flex flex-row-reverse max-w-full items-center p-5 sm:p-10 justify-around gap-3">
-      <div className="m-auto relative">
+    <div className="lg:flex flex-row max-w-full items-center p-5 sm:p-10 justify-around gap-3">
+      <div className="m-auto relative ">
         <h1 className="m-auto text-2xl font-extrabold md:text-4xl lg:text-5xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-sky-400">
             Senior
@@ -16,6 +16,7 @@ const Section = () => {
           </span>
         </h1>
       </div>
+      <img className="rounded-t-lg" src={avatar} alt="avatar" width="200px" height="200px"/>
     </div>
   );
 };
